7265c226b8cf674032fb53aad0d630df6b4c954b
Pedro Albea <pedro@pedroalbea.com>
2017-10-14 02:49:53 -0500

    Added spawning zombies accumulation of rage and fear
    Added Van Helsing killing a zombie
    Rage increases more when there are no zombies available
    Fear increases faster the more zombies you have

--- something something zombies/something something zombies/GameScene.swift
+++ something something zombies/something something zombies/GameScene.swift
@@ -11,85 +11,104 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     private var tapCount: Int = 0
+    private var fear: Int = 50
+    private var rage: Int = 0
+    private var raiseDeadButton: SKLabelNode?
+    private var rageLabel: SKLabelNode?
+    private var fearLabel: SKLabelNode?
+    private var fearCharge: Double = 0
+    private var zombies: [SKLabelNode] = [SKLabelNode]()
+    private var timer: Double?
+    private var vanHelsing: Double = 0
+    private var rageMomentum: Double = 1
+    
     var viewController: GameViewController?
     
     override func didMove(to view: SKView) {
+        self.raiseDeadButton = self.childNode(withName: "//raiseDead") as? SKLabelNode
+        self.fearLabel = self.childNode(withName: "//fearCount") as? SKLabelNode
+        self.rageLabel = self.childNode(withName: "//rageCount") as? SKLabelNode
+    }
+    
+    func raiseDead() {
+        if self.fear < 10 {
+            return
+        }
+        self.fear = self.fear - 10
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        tapCount += 1
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-        if tapCount > 20 && viewController != nil {
-            viewController!.endGame()
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let zombie: SKLabelNode = SKLabelNode()
+        zombie.text = "Z"
+        zombie.fontColor = SKColor.green
+        zombie.fontName = "Helvetica"
+        zombie.fontSize = 48
+        //zombie position is based on the center of the screen so get the random x coordinate and then subtract it by half the screen width to get the right position. Same with y.
+        let randomPosition = CGPoint(x:CGFloat(arc4random()).truncatingRemainder(dividingBy: size.width) - size.width / 2,
+                                     y: CGFloat(arc4random()).truncatingRemainder(dividingBy: size.height) - size.height / 2)
+        zombie.position = randomPosition
+        self.addChild(zombie)
+        self.zombies.append(zombie)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        var touch: CGPoint
+        for t in touches {
+            touch = t.location(in: self)
+            for n in self.nodes(at: touch) {
+                if n == self.raiseDeadButton {
+                    raiseDead()
+                }
+            }
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+        
     override func update(_ currentTime: TimeInterval) {
+        if self.timer == nil {
+            self.timer = currentTime
+        }
+        
+        if currentTime - self.timer! > 0.1 {
+            fearCharge += 0.05 * Double(self.zombies.count)
+            if fearCharge >= 1.0 && fear < 101 { //Each monster contributes to incrementing fear amount. That's the fearCharge. When fearCharge is greater than 1, we actually increment the fear amount
+                fear += 1
+                fearCharge = fearCharge - 1.0
+            }
+            
+            //Handle Van Helsing killing zombies
+            vanHelsing += 1
+            if (vanHelsing > 50) {
+                if zombies.count > 0 {
+                    rageMomentum = 1.0
+                } else {
+                    rageMomentum = rageMomentum * 1.5 //Rage increases faster if there are no monsters to fight
+                }
+                rage = Int(Double(rage) + rageMomentum)
+                vanHelsing = 0
+                if zombies.count > 0 {
+                    let killedZombie = zombies.popLast()
+                    self.removeChildren(in: [killedZombie!])
+                }
+            }
+            
+            self.timer = currentTime
+        }
+        
+        //TODO: alter end game message based on end condition
+        if fear >= 100 && self.viewController != nil {
+            self.viewController!.endGame()
+        }
+        
+        //TODO: alter end game message based on end condition
+        if rage >= 100 && self.viewController != nil {
+            self.viewController!.endGame()
+        }
+        
         // Called before each frame is rendered
+        if self.fearLabel != nil {
+            self.fearLabel!.text = String(self.fear)
+        }
+        if self.rageLabel != nil {
+            self.rageLabel!.text = String(self.rage)
+        }
     }
 }
