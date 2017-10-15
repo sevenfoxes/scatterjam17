9faa4c2b28b24f56ad24dac7ddf9a2cdd2f1abc6
Scott Nesham <scott.nesham@asynchrony.com>
2017-10-14 23:01:45 -0500

    add a background image

--- something something zombies/something something zombies/GameViewController.swift
+++ something something zombies/something something zombies/GameViewController.swift
@@ -46,6 +46,10 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let uicolor = UIColor(patternImage: UIImage(named: "grass")!)
+        self.view.backgroundColor = uicolor
+    
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'StartScreen.sks'
             if let scene = StartScreen(fileNamed: "StartScreen") {
