91e185de31eb6173f267ef0afc59ff93f6cfeb17
Scott Nesham <scott.nesham@asynchrony.com>
2017-10-15 00:49:14 -0500

    add some grass for your ass

--- something something zombies/something something zombies/GameViewController.swift
+++ something something zombies/something something zombies/GameViewController.swift
@@ -45,9 +45,6 @@ class GameViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let uicolor = UIColor(patternImage: UIImage(named: "grass")!)
-        self.view.backgroundColor = uicolor
     
         
         if let view = self.view as! SKView? {
